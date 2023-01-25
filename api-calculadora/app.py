a521004d767d351f7bd890df247c1e07f56aab53
Raony <raonysalim@gmail.com>
2023-01-25 17:58:09 -0300

    Projeto finalizado

---
+++ api-calculadora/app.py
@@ -0,0 +1,61 @@
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+
+
+app = Flask(__name__)
+CORS(app)
+
+def isfloat(num):
+    try:
+        float(num)
+        return True
+    except ValueError:
+        return False
+
+def calcHipotenusa (num1,num2):
+    return (((num1 ** 2) + (num2 ** 2)) ** (1/2))
+
+def calcCateto (num1,num2):
+    return (((num1 ** 2) - (num2 ** 2)) ** (1/2))
+
+  
+@app.route('/', methods= ['POST'])
+def calculadora():
+        body = request.get_json()
+        body = body['data']
+        cateto1 = body['c1']
+        cateto2 = body['c2']
+        hipotenusa = body['h']
+        cateto1 = float(cateto1) if isfloat(cateto1) else 0
+        cateto2 = float (cateto2) if isfloat(cateto2) else 0
+        hipotenusa = float (hipotenusa)   if isfloat(hipotenusa) else 0
+        if (hipotenusa == 0): 
+            resultTemp = calcHipotenusa(cateto1,cateto2)
+            resultDic = {
+                'h': resultTemp,
+                'c1':cateto1,
+                'c2':cateto2,
+                'result': resultTemp
+            }
+            return jsonify(resultDic)
+        elif (cateto1 == 0) :
+            resultTemp = calcCateto(hipotenusa,cateto2)
+            resultDic = {
+                'c1': resultTemp,
+                'h':hipotenusa,
+                'c2':cateto2,
+                'result': resultTemp
+            }
+            return jsonify(resultDic)
+        else:
+            resultTemp = calcCateto(hipotenusa,cateto1)
+            resultDic = {
+                'c2': resultTemp,
+                'c1':cateto1,
+                'h':hipotenusa,
+                'result': resultTemp
+            }
+            return jsonify(resultDic)
+
+
+app.run(port=5000, host='localhost', debug=True)
---
+++ calculadora_pitagoras/src/App.jsx
@@ -0,0 +1,125 @@
+import axios from "axios";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+
+const checkHipotenusa = (c, h) => {
+  Number(c);
+  Number(h);
+  if (Number(c) > Number(h)) {
+    return true;
+  }
+  return false;
+};
+
+function App() {
+  const { register, handleSubmit } = useForm();
+
+  const [result, setResult] = useState(0);
+  const [blockBtn, setBlockBtn] = useState("btn");
+  const [hnum, setHnum] = useState(0);
+  const [c1num, setC1num] = useState(0);
+  const [c2num, setC2num] = useState(0);
+  const [disableNum, setDisableNum] = useState("black");
+
+  let count = 0;
+
+  const onSubmit = ({ c1, c2, h }) => {
+    setResult("Calculando...");
+    setBlockBtn("disable");
+    setDisableNum("black");
+    if (c1 <= 0) {
+      c1 = "0";
+      count++;
+    }
+
+    if (c2 <= 0) {
+      c2 = "0";
+      count++;
+    }
+
+    if (h <= 0) {
+      h = "0";
+      count++;
+    }
+
+    if (count != 1) {
+      setResult("Preencha os dois campos");
+      setTimeout(() => {
+        setBlockBtn("btn");
+      }, 500);
+      return;
+    }
+
+    if (c1 <= 0 || c2 <= 0) {
+      if (checkHipotenusa(c1, h) || checkHipotenusa(c2, h)) {
+        count == 1
+          ? setResult("A Hipotenusa não pode ser menor que o Cateto")
+          : setResult("Preencha os dois campos");
+        setTimeout(() => {
+          setBlockBtn("btn");
+        }, 500);
+        return;
+      }
+    }
+
+    axios
+      .post("http://localhost:5000", {
+        data: {
+          c1,
+          c2,
+          h,
+        },
+      })
+      .then(({ data }) => {
+        setResult(data.result);
+        setC1num(data.c1);
+        setC2num(data.c2);
+        setHnum(data.h);
+        setDisableNum("");
+        setBlockBtn("btn");
+      })
+      .catch((e) => {});
+  };
+
+  return (
+    <div>
+      <h1>Teorema de Pitagoras</h1>
+      <div className="container">
+        <form onSubmit={handleSubmit(onSubmit)} className="form">
+          <div>
+            <label>Cateto</label>
+            <input type="number" {...register("c1")} min="0" step="0.01" />
+          </div>
+          <div>
+            <label>Cateto</label>
+            <input type="number" {...register("c2")} min="0" step="0.01" />
+          </div>
+          <div>
+            <label>Hipotenusa</label>
+            <input type="number" {...register("h")} min="0" step="0.01" />
+          </div>
+          <button className={blockBtn} type="submit">
+            Calcular
+          </button>
+        </form>
+        <div className="result">
+          {isNaN(result) ? result : result.toFixed(4)}
+        </div>
+        <div className="containerImage">
+          <div className="hipotenusa">
+            Hipotenusa <span className={disableNum}>{hnum.toFixed(2)}</span>
+          </div>
+          <p className="cateto1">
+            Cateto <span className={disableNum}>{c1num.toFixed(2)}</span>
+          </p>
+          <p className="cateto2">
+            Cateto <span className={disableNum}>{c2num.toFixed(2)}</span>
+          </p>
+          <div className="image"></div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
